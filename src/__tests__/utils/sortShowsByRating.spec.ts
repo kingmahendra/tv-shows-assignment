60d75dcb16d1535f8eca24da3182d56b8a54bf67
Mahendra Kumar <masterkingmahendra@gmail.com>
2026-02-20 15:33:41 +0100

    fixed path in spec file

--- src/__tests__/utils/sortShowsByRating.spec.ts
+++ src/__tests__/utils/sortShowsByRating.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { sortShowsByRating } from '@/utils/sortShowsByRating'
+import { sortShowsByRating } from '../../utils/sortShowsByRating'
 import { createMockShow } from '../testUtils'
 
 describe('sortShowsByRating', () => {
