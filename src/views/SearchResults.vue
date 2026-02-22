66d446db22744699eaaafca57eb2b2f306f156b7
Mahendra Kumar <masterkingmahendra@gmail.com>
2026-02-22 13:43:09 +0100

    added css loader for loading states

--- src/views/SearchResults.vue
+++ src/views/SearchResults.vue
@@ -15,8 +15,7 @@ const { loading, error, searchResults } = storeToRefs(showsStore)
       <GoBack />
     </div>
 
-    <div v-if="loading" class="loading">
-      <p>Searching for shows...</p>
+    <div v-if="loading" class="loader">
     </div>
 
     <div v-else-if="error" class="error">
@@ -58,7 +57,6 @@ h1 {
   text-align: center;
 }
 
-.loading,
 .error,
 .no-results {
   text-align: center;
@@ -92,6 +90,24 @@ h1 {
   width: 100%;
   max-width: 320px;
 }
+
+.loader {
+  margin: 0 auto;
+  width: 50px;
+  padding: 8px;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  background: #25b09b;
+  --_m: 
+    conic-gradient(#0000 10%,#000),
+    linear-gradient(#000 0 0) content-box;
+  -webkit-mask: var(--_m);
+          mask: var(--_m);
+  -webkit-mask-composite: source-out;
+          mask-composite: subtract;
+  animation: l3 1s infinite linear;
+}
+@keyframes l3 {to{transform: rotate(1turn)}}
 
 @media (max-width: 768px) {
   .search-results {
--- src/views/ShowDetails.vue
+++ src/views/ShowDetails.vue
@@ -18,8 +18,7 @@ const { loading, error, selectedShow } = storeToRefs(showsStore)
 
 <template>
   <div class="show-details">
-    <div v-if="loading" class="loading">
-      <p>Loading show details...</p>
+    <div v-if="loading" class="loader">
     </div>
 
     <div v-else-if="error" class="error">
@@ -56,7 +55,6 @@ const { loading, error, selectedShow } = storeToRefs(showsStore)
   max-width: 210px;
 }
 
-.loading,
 .error,
 .show-not-found {
   text-align: center;
@@ -73,4 +71,22 @@ const { loading, error, selectedShow } = storeToRefs(showsStore)
 .show-not-found p {
   color: #7f8c8d;
 }
+
+.loader {
+  margin: 0 auto;
+  width: 50px;
+  padding: 8px;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  background: #25b09b;
+  --_m: 
+    conic-gradient(#0000 10%,#000),
+    linear-gradient(#000 0 0) content-box;
+  -webkit-mask: var(--_m);
+          mask: var(--_m);
+  -webkit-mask-composite: source-out;
+          mask-composite: subtract;
+  animation: l3 1s infinite linear;
+}
+@keyframes l3 {to{transform: rotate(1turn)}}
 </style>
